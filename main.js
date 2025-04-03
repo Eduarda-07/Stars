5a99c1d914e2f5d3619f41845681883e5af027d3
EDUARDA DE JESUS SILVA <24122366@CFP127ALUNO.LOCAL>
2025-04-03 17:02:14 -0300

    tela de cadastro, recuperação de senha, e nova senha

--- main.js
+++ main.js
@@ -26,10 +26,123 @@ async function login(){
     if(response.status == 200){
         alert('Login realizado com sucesso!')
 
-        window.location.href = "./index2.html"
+        window.location.href = "./home.html"
+        
     }else{
         alert('Login ou senha invalida')
-
     }
 
 }
+
+async function registrar(){
+    const url = `https://back-spider.vercel.app/user/cadastrarUser`
+
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('senha').value
+    const premium = document.getElementById('premium').value
+    const imagem = document.getElementById('imagemPerfil').value
+    const recuperacao = document.getElementById('senhaRecuperacao').value
+
+    
+
+    const data = {
+        nome: nome,
+        email: email,
+        senha: senha,
+        premium: premium,
+        imagemPerfil: imagem,
+        senhaRecuperacao: recuperacao
+
+    }
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(url,options)
+
+    console.log(response);
+
+    if(response.status == 201){
+        alert('Cadastro realizado com sucesso!')
+         window.location.href = "./login.html"
+
+    }else{
+        alert('Cadastro inválido.')
+    }
+
+}
+
+async function recuperacao(){
+    const url = `https://back-spider.vercel.app/user/RememberPassword`
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('senha').value
+    
+
+    const data = {
+        email: email,
+        wordKey: password
+    }
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }
+
+    if(response.status == 200){
+        alert('Palavra verificada!')
+
+        window.location.href = "./novaSenha.html"
+        
+    }else{
+        alert('Palavra invalida')
+    }
+
+}
+async function novaSenha(dadosID){
+    const dados = await response.json()
+   
+    const dadosID = dados.id
+    console.log( dadosID);
+
+    const url = `https://back-spider.vercel.app/user/newPassword/:${dadosID}`
+
+    const password = document.getElementById('senha').value
+    
+
+    const data = {
+        senha: password
+    }
+    const options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(url,options)
+
+    // console.log(response);
+    console.log( dadosID);
+
+    if(response.status == 200){
+        alert('Senha atualizada com sucesso!')
+
+        window.location.href = "./login.html"
+        
+    }else{
+        alert('Não foi possível atualizar a senha')
+    }
+
+}
+
+
+
+
+
