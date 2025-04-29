e78deff78fc749ee54683d530a4f2f28af4b8041
EDUARDA DE JESUS SILVA <24122366@CFP127ALUNO.LOCAL>
2025-04-29 17:05:58 -0300

    tela home

--- main.js
+++ main.js
@@ -31,7 +31,10 @@ async function login() {
          // Armazenar no local Storage
     localStorage.setItem("dadosIdLogado", dataResponse.user.id)
 
-        window.location.href = "./perfil.html"
+    //     window.location.href = "./perfil.html"
+
+    window.location.href = "./home.html"
+
 
     } else {
         alert('Login ou senha invalida')
@@ -153,6 +156,7 @@ async function novaSenha() {
 }
 
 async function perfil() {
+    window.location.href = "./perfil.html"
     const dadosIDNumber = localStorage.getItem("dadosIdLogado")
     console.log(dadosIDNumber);
 
@@ -161,7 +165,7 @@ async function perfil() {
     const data = await fetch(url)
     const json1 = await data.json()
     telaPerfil(json1)
-
+  
 }
 
 async function publicacoes() {
@@ -286,7 +290,82 @@ async function telaPerfil(json) {
 
 // window.onload = perfil;
 
-perfil()
+// perfil()
 
 publicacoes()
 
+
+
+
+
+
+async function pesquisarStories(){
+    const galeria = document.getElementById('galeria')
+    const url = 'https://back-spider.vercel.app/storys/listarStorys' // url da api 
+
+    const response  = await fetch(url) // fetch -> faz requisições web (conversa com o back)
+    const data      = await response.json() // chamar apenas o json
+    return data
+}
+
+// Função para criar as imgs dentro da DIV 
+function criarStories(link){ // Recebe o link da imagem 
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img') // criando nova imagem 
+    novaImg.src = link.imagem// Cria a nova image, com a link da foto do API 
+
+    galeria.appendChild(novaImg)
+}
+
+async function pesquisarPublicacao(){
+    const url = 'https://back-spider.vercel.app/publicacoes/listarPublicacoes' // url da api 
+
+    const response  = await fetch(url) // fetch -> faz requisições web (conversa com o back)
+    const data      = await response.json() // chamar apenas o json
+    return data
+}
+
+function criarPost(link){ // Recebe o link da imagem 
+    const post = document.getElementById('fotoPost')
+    const novaImg = document.createElement('img') // criando nova imagem 
+    const local = document.createElement ('h2')
+    const postagem = document.createElement('div')
+    const comentario = document.createElement('h3')
+    const data = document.createElement ('p')
+    const user = document.createElement('p')
+    
+    novaImg.src = link.imagem// Cria a nova image, com a link da foto do API 
+    local.textContent = link.local
+    postagem.classList = 'postagem'
+    comentario.textContent = link.descricao 
+    data.textContent = link.dataPublicacao
+    user.textContent = link.id   
+
+postagem.appendChild(user)
+postagem.appendChild(local)
+postagem.appendChild(novaImg)
+postagem.appendChild(comentario)
+postagem.appendChild(data)
+post.appendChild(postagem)
+}
+
+function comentarios(){
+    const icon = document.createElement('div')
+    icon.className = 'icons'
+
+    const comentario = document.createElement('img')
+        comentario.src = 'https://cdn-icons-png.flaticon.com/512/1380/1380338.png'
+        icon.appendChild(comentario)
+}
+
+
+
+async function preencherFotos(){
+    const storys = await pesquisarStories()
+    const publication = await pesquisarPublicacao()
+
+    storys.forEach (criarStories)
+    publication.forEach(criarPost)
+}
+
+preencherFotos()
